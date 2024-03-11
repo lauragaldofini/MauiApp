538026bc050ae703b57b92a5b74b64c6c3000060
Usuario <Usuario@CPD03-M05>
2024-03-11 15:59:41 -0300

    Laura

--- MauiApp/MainPage.xaml.cs
+++ MauiApp/MainPage.xaml.cs
@@ -2,13 +2,36 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        string vez = "0";
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
-    }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+            disparador.IsEnabled = false;
 
-}
+            if (vez = = "0")
+            {
+                disparador.Text = "0";
+                vez = "X";
+            }
+            else
+            {
+                disparador.Text = "X";
+                vez = "0";
+            }
+
+            // verificando se o "X" ganhou na primeira linha
+
+            if (bnt10.Text == "X" &&
+                bnt11.Text == "X" &&
+                bnt12.Text == "X")
+
+            }
+            
+            if (bnt20.Text == "X" &&
+            bnt21.Text == "X" && 
+            bnt22.Text == "X");
